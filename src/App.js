0cb7098cb8930569a7a766f2e7b0ecee80fed5d2
chrisli5 <chrisli41@gmail.com>
2020-11-20 00:09:20 -0800

    Add Header component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,12 @@
 import React from 'react';
+import { BrowserRouter as Router, Switch } from 'react-router-dom';
 import Home from './pages/home';
 
 export default function App() {
-  return <Home />;
+  
+  return (
+    <Router>
+      <Home />
+    </Router>
+  )
 }
