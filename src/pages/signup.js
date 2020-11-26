cb3b39887ccb71bd290843a71ea394eec6c03dfc
chrisli5 <chrisli41@gmail.com>
2020-11-26 10:05:42 -0800

    Add Sign Up component

--- src/pages/signup.js
+++ src/pages/signup.js
@@ -1,5 +1,33 @@
-import React from 'react';
+import React, { useState, useContext } from 'react';
+import { useHistory } from 'react-router-dom';
+import { HeaderContainer } from '../containers/header';
+import { FooterContainer } from '../containers/footer';
+import { Form } from '../components';
+import { FirebaseContext } from '../context/firebase';
+import * as ROUTES from '../constants/routes';
+import { Header } from '../components/accordion/styles/accordion';
 
 export default function SignUp() {
-  return <p>SignUp Page</p>;
+  const history = useHistory();
+  const { firebase } = useContext(FirebaseContext);
+
+  const [firstName, setFirstName] = useState('');
+  const [emailAddress, setEmailAddress] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState('');
+
+  const isInvalid = firstName === '' || password === '' || emailAddress === '';
+
+  const handleSignUp = (event) => {
+    event.preventDefault();
+  };
+
+  return (
+    <HeaderContainer>
+      <Form>
+        <Form.Title>Sign Up</Form.Title>
+        {error && <Form.Error>{error}</Form.Error>}
+      </Form>
+    </HeaderContainer>
+  )
 }
